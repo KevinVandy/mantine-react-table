72ea70b62a6ccff961b2ce9c67df9b2e1ed3093b
Kevin Vandy <kevinvandy656@gmail.com>
2023-02-07 23:51:51 -0600

    add sv locale

---
+++ apps/mantine-react-table-docs/examples/localization-i18n-sv/sandbox/src/TS.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+//Import Mantine React Table and its Types
+import { MantineReactTable, MRT_ColumnDef } from 'mantine-react-table';
+
+//Import Mantine React Table Translations
+import { MRT_Localization_SV } from 'mantine-react-table/locales/sv';
+
+//mock data
+import { data, Person } from './makeData';
+
+const columns: MRT_ColumnDef<Person>[] = [
+  //column definitions...
+  {
+    accessorKey: 'firstName',
+    header: 'Förnamn',
+  },
+  {
+    accessorKey: 'lastName',
+    header: 'Efternamn',
+    enableClickToCopy: true,
+  },
+  {
+    accessorKey: 'age',
+    header: 'Ålder',
+  },
+  //end
+];
+
+const Example = () => {
+  return (
+    <MantineReactTable
+      columns={columns}
+      data={data}
+      enableColumnFilterModes
+      enableColumnOrdering
+      enableEditing
+      enablePinning
+      enableRowActions
+      enableRowSelection
+      enableSelectAll={false}
+      initialState={{ showColumnFilters: true, showGlobalFilter: true }}
+      localization={MRT_Localization_SV}
+    />
+  );
+};
+
+export default Example;
