0f872b5f5e424b87cd5c52783c750c82f81c0bd8
Kevin Vandy <kevinvandy656@gmail.com>
2023-06-28 21:40:28 -0500

    update usage docs

--- apps/mantine-react-table-docs/examples/SimpleCodeSandbox.tsx
+++ apps/mantine-react-table-docs/examples/SimpleCodeSandbox.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useThemeContext } from '../styles/ThemeContext';
 
 const SimpleCodeSandbox = () => {
+  const { isLightTheme } = useThemeContext();
+
   return (
     <iframe
-      src="https://codesandbox.io/embed/simple-mantine-react-table-example-t5c3ji?fontsize=14&hidenavigation=1&theme=dark"
+      src={`https://codesandbox.io/embed/simple-mantine-react-table-y2kztl?fontsize=14&hidenavigation=1&theme=${
+        isLightTheme ? 'light' : 'dark'
+      }`}
       style={{
         width: '100%',
         height: '500px',
@@ -11,7 +16,7 @@ const SimpleCodeSandbox = () => {
       title="simple-mantine-react-table-example"
       allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
       sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
-    />
+    ></iframe>
   );
 };
 
