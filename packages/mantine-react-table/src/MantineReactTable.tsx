2f6481eae09ea46c80f9b2b17e640de45eaa330d
Kevin Vandy <kevinvandy656@gmail.com>
2023-06-30 21:27:33 -0500

    better xor prop types on MantineReactTable

--- packages/mantine-react-table/src/MantineReactTable.tsx
+++ packages/mantine-react-table/src/MantineReactTable.tsx
@@ -1,15 +1,21 @@
-import { MRT_TablePaper } from './table/MRT_TablePaper';
 import { useMantineReactTable } from './useMantineReactTable';
+import { MRT_TablePaper } from './table/MRT_TablePaper';
 import { type MRT_TableOptions, type MRT_TableInstance } from './types';
-import { useEffect, useRef } from 'react';
+
+type Prettify<T> = { [K in keyof T]: T[K] } & unknown;
+
+type Xor<A, B> =
+  | Prettify<A & { [k in keyof B]?: never }>
+  | Prettify<B & { [k in keyof A]?: never }>;
 
 type TableInstanceProp<TData extends Record<string, any>> = {
   table: MRT_TableInstance<TData>;
 };
 
-type Props<TData extends Record<string, any>> =
-  | TableInstanceProp<TData>
-  | MRT_TableOptions<TData>;
+type Props<TData extends Record<string, any>> = Xor<
+  TableInstanceProp<TData>,
+  MRT_TableOptions<TData>
+>;
 
 const isTableInstanceProp = <TData extends Record<string, any>>(
   props: Props<TData>,
@@ -26,24 +32,6 @@ export const MantineReactTable = <TData extends Record<string, any>>(
   } else {
     table = useMantineReactTable(props);
   }
-
-  const initialBodyHeight = useRef<string>();
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      initialBodyHeight.current = document.body.style.height;
-    }
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      if (table.getState().isFullScreen) {
-        document.body.style.height = '100vh';
-      } else {
-        document.body.style.height = initialBodyHeight.current as string;
-      }
-    }
-  }, [table.getState().isFullScreen]);
 
   return <MRT_TablePaper table={table} />;
 };
