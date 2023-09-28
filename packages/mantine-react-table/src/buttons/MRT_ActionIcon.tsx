148107795907f8c121dbea2b9120f64db3738195
Patrick Naughton <patrickjnaughton@gmail.com>
2023-09-27 17:16:51 -0700

    do pinning buttons

--- packages/mantine-react-table/src/buttons/MRT_ActionIcon.tsx
+++ packages/mantine-react-table/src/buttons/MRT_ActionIcon.tsx
@@ -3,8 +3,11 @@ import { type PropsWithChildren } from 'react';
 import { ActionIcon, type ActionIconProps } from '@mantine/core';
 
 import classes from './MRT_ActionIcon.module.css';
+import { type PolymorphicComponentProps } from '@mantine/core/lib/core/factory/create-polymorphic-component';
 
-type Props = PropsWithChildren<ActionIconProps>;
+type Props<C = 'button'> = PropsWithChildren<
+  PolymorphicComponentProps<C, ActionIconProps>
+>;
 
 export function MRT_ActionIcon({
   children,
