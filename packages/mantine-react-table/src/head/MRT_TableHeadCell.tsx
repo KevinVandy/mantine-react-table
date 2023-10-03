8612acdf05f305bef6116cd45925f7ee6bc3f58f
Kevin Van Cott <kevinvandy656@gmail.com>
2023-10-03 15:49:51 -0500

    refactor table head cell css

--- packages/mantine-react-table/src/head/MRT_TableHeadCell.tsx
+++ packages/mantine-react-table/src/head/MRT_TableHeadCell.tsx
@@ -1,15 +1,26 @@
-import { type DragEvent, type ReactNode, useMemo } from 'react';
-import { Flex, TableTh, useMantineTheme } from '@mantine/core';
-import clsx from 'clsx';
+import {
+  type DragEvent,
+  type ReactNode,
+  useMemo,
+  type CSSProperties,
+} from 'react';
+import { Box, Flex, useMantineTheme } from '@mantine/core';
 import { MRT_ColumnActionMenu } from '../menus/MRT_ColumnActionMenu';
 import { MRT_TableHeadCellFilterContainer } from './MRT_TableHeadCellFilterContainer';
 import { MRT_TableHeadCellFilterLabel } from './MRT_TableHeadCellFilterLabel';
 import { MRT_TableHeadCellGrabHandle } from './MRT_TableHeadCellGrabHandle';
 import { MRT_TableHeadCellResizeHandle } from './MRT_TableHeadCellResizeHandle';
 import { MRT_TableHeadCellSortLabel } from './MRT_TableHeadCellSortLabel';
-import { parseFromValuesOrFunc } from '../column.utils';
+import {
+  getIsFirstRightPinnedColumn,
+  getIsLastLeftPinnedColumn,
+  getPrimaryColor,
+  getTotalRight,
+  parseCSSVarId,
+  parseFromValuesOrFunc,
+} from '../column.utils';
 import { type MRT_Header, type MRT_TableInstance } from '../types';
-
+import clsx from 'clsx';
 import classes from './MRT_TableHeadCell.module.css';
 
 interface Props<TData extends Record<string, any> = {}> {
@@ -48,6 +59,21 @@ export const MRT_TableHeadCell = <TData extends Record<string, any> = {}>({
     ...parseFromValuesOrFunc(columnDef.mantineTableHeadCellProps, arg),
   };
 
+  const widthStyles = useMemo(() => {
+    const styles: CSSProperties = {
+      minWidth: `max(calc(var(--col-${parseCSSVarId(column.id)}-size) * 1px), ${
+        column.columnDef.minSize ?? 30
+      }px)`,
+      width: `calc(var(--col-${parseCSSVarId(column.id)}-size) * 1px)`,
+    };
+
+    if (layoutMode === 'grid') {
+      styles.flex = `${column.getSize()} 0 auto`;
+    }
+
+    return styles;
+  }, [column]);
+
   const showColumnActions =
     (enableColumnActions || columnDef.enableColumnActions) &&
     columnDef.enableColumnActions !== false;
@@ -72,9 +98,9 @@ export const MRT_TableHeadCell = <TData extends Record<string, any> = {}>({
   const draggingBorder = useMemo(
     () =>
       draggingColumn?.id === column.id
-        ? `1px dashed var(--mantine-color-gray-7) !important`
+        ? `1px dashed ${theme.colors.gray[7]} !important`
         : hoveredColumn?.id === column.id
-        ? `2px dashed var(--mantine-color-gray-7) !important`
+        ? `2px dashed ${getPrimaryColor(theme)} !important`
         : undefined,
     [draggingColumn, hoveredColumn],
   );
@@ -85,7 +111,7 @@ export const MRT_TableHeadCell = <TData extends Record<string, any> = {}>({
         borderRight: draggingBorder,
         borderTop: draggingBorder,
       }
-    : {};
+    : undefined;
 
   const handleDragEnter = (_e: DragEvent) => {
     if (enableGrouping && hoveredColumn?.id === 'drop-zone') {
@@ -99,86 +125,107 @@ export const MRT_TableHeadCell = <TData extends Record<string, any> = {}>({
   };
 
   const headerElement =
-    parseFromValuesOrFunc(columnDef?.Header, {
-      column,
-      header,
-      table,
-    }) ?? (columnDef.header as ReactNode);
+    columnDef?.Header instanceof Function
+      ? columnDef?.Header?.({
+          column,
+          header,
+          table,
+        })
+      : columnDef?.Header ?? (columnDef.header as ReactNode);
 
   return (
-    <TableTh
+    <Box
+      component="th"
       align={columnDefType === 'group' ? 'center' : 'left'}
-      data-ispinned={column?.getIsPinned() ?? 'false'}
-      data-cansort={column.getCanSort()}
-      data-isresizing={column.getIsResizing()}
       colSpan={header.colSpan}
       onDragEnter={handleDragEnter}
-      data-columndef={columnDefType}
       ref={(node: HTMLTableCellElement) => {
         if (node) {
           tableHeadCellRefs.current[column.id] = node;
         }
       }}
       {...tableCellProps}
-      className={clsx(classes.MRT_TableHeadCell, tableCellProps.className)}
       __vars={{
-        '--flex-direction': layoutMode === 'grid' ? 'column' : undefined,
-        '--padding':
-          density === 'xl' ? '23px' : density === 'md' ? '16px' : '10px',
-        '--user-select':
-          enableMultiSort && column.getCanSort() ? 'none' : undefined,
-        '--z-index':
+        '--mrt-table-head-cell-padding':
+          density === 'xl' ? '23' : density === 'md' ? '16' : '10',
+        '--mrt-table-head-cell-z-index':
           column.getIsResizing() || draggingColumn?.id === column.id
             ? '3'
             : column.getIsPinned() && columnDefType !== 'group'
             ? '2'
             : '1',
-        ...tableCellProps.__vars,
+        '--mrt-table-cell-left':
+          column.getIsPinned() === 'left'
+            ? `${column.getStart('left')}`
+            : undefined,
+        '--mrt-table-cell-right':
+          column.getIsPinned() === 'right'
+            ? `${getTotalRight(table, column)}`
+            : undefined,
       }}
-      style={{
+      className={clsx(
+        'mrt-table-head-cell',
+        classes.root,
+        layoutMode === 'grid' && classes['root-grid'],
+        enableMultiSort && column.getCanSort() && classes['root-no-select'],
+        column.getIsPinned() &&
+          column.columnDef.columnDefType !== 'group' &&
+          classes['root-pinned'],
+        column.getIsPinned() === 'left' && classes['root-pinned-left'],
+        column.getIsPinned() === 'right' && classes['root-pinned-right'],
+        getIsLastLeftPinnedColumn(table, column) &&
+          classes['root-pinned-left-last'],
+        getIsFirstRightPinnedColumn(column) &&
+          classes['root-pinned-right-first'],
+        tableCellProps?.className,
+        draggingColumn?.id === column.id ||
+          (table.getState().hoveredColumn?.id === column.id &&
+            classes['root-opacity']),
+      )}
+      style={(theme) => ({
+        ...parseFromValuesOrFunc(tableCellProps?.style, theme),
+        ...widthStyles,
         ...draggingBorders,
-      }}
+      })}
     >
       {header.isPlaceholder ? null : (
         <Flex
-          className={classes.content}
-          __vars={{
-            '--justify-content':
-              columnDefType === 'group' || tableCellProps?.align === 'center'
-                ? 'center'
-                : column.getCanResize()
-                ? 'space-between'
-                : 'flex-start',
-            '--flex-direction':
-              tableCellProps?.align === 'right' ? 'row-reverse' : 'row',
-          }}
+          className={clsx(
+            'mrt-table-head-cell-content',
+            classes.content,
+            (columnDefType === 'group' || tableCellProps?.align === 'center') &&
+              classes['content-center'],
+            tableCellProps?.align === 'right' && classes['content-right'],
+            column.getCanResize() && classes['content-spaced'],
+          )}
         >
           <Flex
-            className={classes.labels}
-            onClick={column.getToggleSortingHandler()}
             __vars={{
-              '--labels-cursor':
-                column.getCanSort() && columnDefType !== 'group'
-                  ? 'pointer'
-                  : undefined,
-              '--labels-flex-direction':
-                tableCellProps?.align === 'right' ? 'row-reverse' : 'row',
-              '--labels-flex-overflow':
-                columnDefType === 'data' ? 'hidden' : undefined,
-              '--labels-padding-left':
-                tableCellProps?.align === 'center'
-                  ? `${headerPL}rem`
-                  : undefined,
+              '--mrt-table-head-cell-labels-padding-left': `${headerPL}`,
             }}
+            className={clsx(
+              'mrt-table-head-cell-labels',
+              classes.labels,
+              column.getCanSort() &&
+                columnDefType !== 'group' &&
+                classes['labels-sortable'],
+              tableCellProps?.align === 'right'
+                ? classes['labels-right']
+                : tableCellProps?.align === 'center' &&
+                    classes['labels-center'],
+              columnDefType === 'data' && classes['labels-data'],
+            )}
+            onClick={column.getToggleSortingHandler()}
           >
             <Flex
-              className={classes.wrapper}
-              __vars={{
-                '--wrapper-overflow':
-                  columnDefType === 'data' ? 'hidden' : undefined,
-                '--wrapper-whitespace':
-                  (columnDef.header?.length ?? 0) < 20 ? 'nowrap' : 'normal',
-              }}
+              className={clsx(
+                'mrt-table-head-cell-content-wrapper',
+                classes['content-wrapper'],
+                columnDefType === 'data' &&
+                  classes['content-wrapper-hidden-overflow'],
+                (columnDef.header?.length ?? 0) < 20 &&
+                  classes['content-wrapper-nowrap'],
+              )}
               title={columnDefType === 'data' ? columnDef.header : undefined}
             >
               {headerElement}
@@ -191,7 +238,12 @@ export const MRT_TableHeadCell = <TData extends Record<string, any> = {}>({
             )}
           </Flex>
           {columnDefType !== 'group' && (
-            <Flex className={classes.actions}>
+            <Flex
+              className={clsx(
+                'mrt-table-head-cell-content-actions',
+                classes['content-actions'],
+              )}
+            >
               {showDragHandle && (
                 <MRT_TableHeadCellGrabHandle
                   column={column}
@@ -214,6 +266,6 @@ export const MRT_TableHeadCell = <TData extends Record<string, any> = {}>({
       {columnFilterDisplayMode === 'subheader' && column.getCanFilter() && (
         <MRT_TableHeadCellFilterContainer header={header} table={table} />
       )}
-    </TableTh>
+    </Box>
   );
 };
