5d50575c161cdfc73e122548c69d74bf78c9dfbc
Kevin Van Cott <kevinvandy656@gmail.com>
2023-10-10 23:30:44 -0500

    release v2.0.0-alpha.1

--- apps/mantine-react-table-docs/pages/_app.tsx
+++ apps/mantine-react-table-docs/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css';
 import '@mantine/code-highlight/styles.css';
 import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
-import 'mantine-react-table/mrt.css';
+import 'mantine-react-table/styles.css';
 import { useState } from 'react';
 import { type AppProps } from 'next/app';
 import Head from 'next/head';
