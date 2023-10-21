b07e06c288a116038d237807d171db4606cb6e04
Kevin Van Cott <kevinvandy656@gmail.com>
2023-10-20 21:15:00 -0500

    release alpha with better esm support

--- apps/mantine-react-table-docs/components/mdx/SampleCodeSnippet.tsx
+++ apps/mantine-react-table-docs/components/mdx/SampleCodeSnippet.tsx
@@ -2,9 +2,15 @@ import {
   CodeHighlight,
   type CodeHighlightProps,
 } from '@mantine/code-highlight';
-import { Paper } from '@mantine/core';
+import { Code, Paper, useMantineTheme } from '@mantine/core';
 
 export function SampleCodeSnippet(props: CodeHighlightProps) {
+  const { primaryColor } = useMantineTheme();
+
+  if (!props.language && !props.className) {
+    return <Code color={primaryColor} fz="0.9em" {...props} />;
+  }
+
   return (
     <Paper radius="sm" shadow="sm" withBorder={props.withCopyButton}>
       <CodeHighlight
