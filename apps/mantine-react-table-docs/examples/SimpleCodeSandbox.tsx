053975d2a537cc0aa3b276c851abad5f219635d2
Kevin Van Cott <kevinvandy656@gmail.com>
2024-01-07 10:54:54 -0600

    remove call

--- apps/mantine-react-table-docs/examples/SimpleCodeSandbox.tsx
+++ apps/mantine-react-table-docs/examples/SimpleCodeSandbox.tsx
@@ -1,12 +1,9 @@
-import { useMantineColorScheme } from '@mantine/core';
 import { useThemeContext } from '../styles/ThemeContext';
 
 const SimpleCodeSandbox = () => {
-  const { colorScheme } = useMantineColorScheme();
-
   return (
     <iframe
-      src={`https://codesandbox.io/embed/simple-mantine-react-table-y2kztl?fontsize=14&hidenavigation=1&theme=${colorScheme}`}
+      src={`https://codesandbox.io/embed/simple-mantine-react-table-y2kztl?fontsize=14&hidenavigation=1`}
       style={{
         width: '100%',
         height: '500px',
