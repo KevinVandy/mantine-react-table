43c6c33623c0dcfb86e72221e1fff0efaa788738
Cedric Dugas <cedric.dugas@gmail.com>
2025-09-30 17:27:35 -0400

    fix showToRow type

--- apps/mantine-react-table-docs/examples/editing-crud-tree/index.tsx
+++ apps/mantine-react-table-docs/examples/editing-crud-tree/index.tsx
@@ -3,12 +3,13 @@ import Example from './sandbox/src/TS';
 
 const TS = require('!!raw-loader!./sandbox/src/TS.tsx').default;
 
-const ExampleTable = () => {
+const ExampleTable = ({ showTopRow = true }) => {
   return (
     <SourceCodeSnippet
       Component={Example}
       typeScriptCode={TS}
       tableId="editing-crud-tree"
+      showTopRow={showTopRow}
     />
   );
 };
